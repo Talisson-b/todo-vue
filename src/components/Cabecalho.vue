aa9376d9a03fede73a0ff614ee5f8cd39ca98d26
Talisson Barbosa <talissonbarbosa@gmail.com>
2023-06-07 17:58:26 -0300

    componetizando a aplicacao

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const props = defineProps(['tarefasPendentes'])
+</script>
+
+
+
+
+<template>
+    <header>
+        <h1 class="p-5 mb-4 mt-4 bg-light rounded-3">Minhas tarefas</h1>
+        <p>
+        Você possui {{ props.tarefasPendentes }} tarefas pendente
+        </p>
+    </header>
+</template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,26 @@
+<script setup>
+    const props = defineProps(['cadastrar', 'tarefaTemp', 'trocarFiltro', 'editaTarefaTemp'])
+</script>
+
+
+<template>
+    <form @submit.prevent="props.cadastrar">
+        <div class="row">
+            <div class="col">
+                <input :value="props.tarefaTemp" @change="editaTarefaTemp" required
+                    type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="trocarFiltro" class="form-control">
+                    <option value="todas">Todas tarefas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas </option>
+
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
